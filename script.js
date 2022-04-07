fe96acffcf2593c65f94ee327f2ce9d8bfa447b8
edward29 <caacbayedward@gmail.com>
2022-04-07 16:42:36 +0800

    init

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ window.onload = function () {
     const btn = document.querySelector("#checkInput");
 
     btn.addEventListener("click", () => {
-        if(Num.value % 2) {
-            document.querySelector("#result").innerHTML = `<p>Your input value is <span>"${Num.value}"</span> </p><p>And this is <span>EVEN</span> Number</p> <p class='name'>Edward Joseph Caacbay, Grade 12 Chronus</p>`;
-        }else{ 
+        if(Num.value % 2 === 0) {
             document.querySelector("#result").innerHTML = `<p>Your input value is <span>"${Num.value}"</span>  </p><p>And this is <span>ODD</span> Number</p> <p class='name'>Edward Joseph Caacbay, Grade 12 Chronus</p>`;
+        }else{ 
+            document.querySelector("#result").innerHTML = `<p>Your input value is <span>"${Num.value}"</span> </p><p>And this is <span>EVEN</span> Number</p> <p class='name'>Edward Joseph Caacbay, Grade 12 Chronus</p>`;
         }
 
         Num.value = "";
