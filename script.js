69c86e66e862e69c53b6cea4e4b8eb31b4910cdf
edward29 <caacbayedward@gmail.com>
2022-04-07 15:25:10 +0800

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    const Num = document.querySelector("#inputNum");
+    const btn = document.querySelector("#checkInput");
+
+    btn.addEventListener("click", () => {
+        if(Num.value % 2) {
+            document.querySelector("#result").innerHTML = `<p>Your input value is <span>"${Num.value}"</span> </p><p>And this is <span>EVEN</span> Number</p> <p class='name'>Edward Joseph Caacbay, Grade 12 Chronus</p>`;
+        }else{ 
+            document.querySelector("#result").innerHTML = `<p>Your input value is <span>"${Num.value}"</span>  </p><p>And this is <span>ODD</span> Number</p> <p class='name'>Edward Joseph Caacbay, Grade 12 Chronus</p>`;
+        }
+
+        Num.value = "";
+    });
+
+}
